4791da7a47d50c0a384109854d5f037cf6b507ce
Maicon <maiconplus@gmail.com>
2023-06-29 20:19:34 -0300

    basic project oop

---
+++ src/br/com/program/Program.java
@@ -0,0 +1,39 @@
+package br.com.program;
+
+import br.com.triangulo.Triangle;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner c = new Scanner(System.in);
+
+        Triangle x, y;
+
+        x = new Triangle();
+        y = new Triangle();
+
+        System.out.println("Enter the measures of tirnagle X:");
+        x.a = c.nextDouble();
+        x.b = c.nextDouble();
+        x.c = c.nextDouble();
+        System.out.println("Enter the measures of triangle Y:");
+        y.a = c.nextDouble();
+        y.b = c.nextDouble();
+        y.c = c.nextDouble();
+
+        double areaX = x.area();
+        double areaY = y.area();
+
+        System.out.printf("Triangle X area: %.4f%n", areaX);
+        System.out.printf("Triangle Y area: %.4f%n", areaY);
+
+
+        c.close();
+
+    }
+
+}
