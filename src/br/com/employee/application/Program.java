e7951e3bcda1d4a9a9be2791e97923cce8ca9f0c
Maicon <maiconplus@gmail.com>
2023-07-08 23:10:16 -0300

    exercise oop project List 2.0

--- src/br/com/employee/application/Program.java
+++ src/br/com/employee/application/Program.java
@@ -13,36 +13,72 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("How many employees will be register? ");
-        int n = sc.nextInt();
-
-
         List<Employee> list = new ArrayList<>();
 
+        System.out.print("How many employees will be registered? ");
+        int N = sc.nextInt();
 
-        for(int i = 0; i <= n; i++){
-            System.out.printf("Employee #", i + 1);
-            System.out.print("ID: ");
-            int id = sc.nextInt();
+        for (int i=0; i<N; i++) {
+
+            System.out.println();
+            System.out.println("Emplyoee #" + (i + 1) + ":");
+            System.out.print("Id: ");
+            Integer id = sc.nextInt();
+            while (hasId(list, id)) {
+                System.out.println("Id already taken! Try again: ");
+                id = sc.nextInt();
+            }
+
             System.out.print("Name: ");
             sc.nextLine();
             String name = sc.nextLine();
             System.out.print("Salary: ");
-            Double sal = sc.nextDouble();
+            Double salary = sc.nextDouble();
 
-            Employee emp = new Employee(id, name, sal);
+            Employee emp = new Employee(id, name, salary);
+
             list.add(emp);
-
         }
 
+        System.out.println();
+        System.out.print("Enter the employee id that will have salary increase : ");
+        int idsalary = sc.nextInt();
 
+        // Integer pos = position(list, idsalary);
 
+        Employee emp = list.stream().filter(x -> x.getId() == idsalary).findFirst().orElse(null);
 
+        // if (pos == null) {
+        if (emp == null) {
+            System.out.println("This id does not exist!");
+        }
+        else {
+            System.out.print("Enter the percentage: ");
+            double percent = sc.nextDouble();
+            //list.get(pos).increaseSalary(percent);
+            emp.increaseSalary(percent);
+        }
 
-
+        System.out.println();
+        System.out.println("List of employees:");
+        for (Employee e : list) {
+            System.out.println(e);
+        }
 
         sc.close();
-
     }
 
+    public static Integer position(List<Employee> list, int id) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getId() == id) {
+                return i;
+            }
+        }
+        return null;
+    }
+
+    public static boolean hasId(List<Employee> list, int id) {
+        Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+        return emp != null;
+    }
 }
--- src/br/com/employee/entities/Employee.java
+++ src/br/com/employee/entities/Employee.java
@@ -3,22 +3,22 @@ package br.com.employee.entities;
 public class Employee {
     private Integer id;
     private String name;
-    private Double Salary;
+    private Double salary;
 
     public Employee() {
     }
 
-    public Employee(int id, String name, double salary) {
+    public Employee(int id, String name, Double salary) {
         this.id = id;
         this.name = name;
-        Salary = salary;
+        this.salary = salary;
     }
 
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -30,11 +30,22 @@ public class Employee {
         this.name = name;
     }
 
-    public double getSalary() {
-        return Salary;
+    public Double getSalary() {
+        return salary;
     }
 
-    public void setSalary(double salary) {
-        Salary = salary;
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public void increaseSalary(double percentage){
+        salary +=  salary * percentage / 100.0;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return id + ", " + name + ", " + String.format("%.2f", salary);
     }
 }
