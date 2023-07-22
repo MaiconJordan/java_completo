7b79717f47dabd1c0b23a2d13380263923ee0aa5
Maicon <maiconplus@gmail.com>
2023-07-21 22:58:50 -0300

    exercise oop Enum Exercise Product ToString

--- src/br/com/exercicioenum/application/Program.java
+++ src/br/com/exercicioenum/application/Program.java
@@ -1,4 +1,13 @@
 package br.com.exercicioenum.application;
 
+import br.com.exercicioenum.entities.OrderItem;
+import br.com.exercicioenum.entities.Product;
+
 public class Program {
+    public static void main(String[] args) {
+        Product p = new Product("TV", 1000.00);
+        OrderItem oi1 = new OrderItem(1, 1000.0, p);
+        System.out.println(oi1);
+
+    }
 }
--- src/br/com/exercicioenum/entities/OrderItem.java
+++ src/br/com/exercicioenum/entities/OrderItem.java
@@ -1,17 +1,15 @@
 package br.com.exercicioenum.entities;
 
-public class OrdermItem {
+public class OrderItem {
     private Integer quantity;
     private Double price;
 
     private Product product;
 
-    public OrdermItem(){
+    public OrderItem(){   }
 
 
-    }
-
-    public OrdermItem(Integer quantity, Double price, Product product) {
+    public OrderItem(Integer quantity, Double price, Product product) {
         this.quantity = quantity;
         this.price = price;
         this.product = product;
@@ -41,6 +39,19 @@ public class OrdermItem {
         this.product = product;
     }
 
-    public
+    public double subTotal(){
+        return price * quantity;
+    }
+
+    @Override
+    public String toString() {
+        return getProduct().getName()
+                + ", $"
+                + String.format("%.2f", price)
+                + ", Quantity:"
+                + quantity
+                + ", Subtotal: $"
+                + String.format("%.2f", subTotal());
+    }
 }
 
