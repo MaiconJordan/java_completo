4f2ce26a03e65aa722526c2e53656c82d2bfb716
Maicon <maiconplus@gmail.com>
2023-07-27 22:05:45 -0300

    exercise oop Herança DOWNCASTING

--- src/br/com/heranca/entities/BusinessAccount.java
+++ src/br/com/heranca/entities/BusinessAccount.java
@@ -1,14 +1,14 @@
 package br.com.heranca.entities;
 
-public class BussinesAccount extends Account {
+public class BusinessAccount extends Account {
 
     private Double loanLimit;
 
-    public BussinesAccount(){
+    public BusinessAccount(){
         super();
     }
 
-    public BussinesAccount(Integer number, String holder, Double balance, Double loanLimit) {
+    public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
         super(number, holder, balance);
         this.loanLimit = loanLimit;
     }
