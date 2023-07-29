df958161af096cd997d025387cd4a6e67942c444
Maicon <maiconplus@gmail.com>
2023-07-28 22:57:08 -0300

    exercise oop Herança Override e Super

--- src/br/com/heranca/application/Program.java
+++ src/br/com/heranca/application/Program.java
@@ -6,7 +6,7 @@ import br.com.heranca.entities.SavingsAccount;
 
 public class Program {
     public static void main(String[] args) {
-
+        /**
         Account acc =new Account(1001, "Alex", 0.0);
 
         BusinessAccount bacc =new BusinessAccount(1008, "Maria", 0.0, 500.00);
@@ -32,6 +32,22 @@ public class Program {
             acc5.updateBalance();
             System.out.println("Update!");
         }
+         **/
+
+        // EX. OVERIDE E SPER
+
+        Account acc1 = new Account(1001, "Alex", 1000.00);
+        acc1.withdraw(200.0);
+        System.out.println(acc1.getBalance());
+
+        Account acc2 = new SavingsAccount(1002, "Maria", 1000.00, 0.01);
+        acc2.withdraw(200.00);
+        System.out.println(acc2.getBalance());
+
+        Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
+        acc3.withdraw(200.0);
+        System.out.println(acc3.getBalance());
+
 
 
     }
