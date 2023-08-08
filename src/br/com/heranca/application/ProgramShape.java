8cb0491eaf6a2460e68afaacc644823603d668a9
Maicon <maiconplus@gmail.com>
2023-08-07 23:03:18 -0300

    Classes abstradas

---
+++ src/br/com/heranca/application/ProgramShape.java
@@ -0,0 +1,18 @@
+package br.com.heranca.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgramShape {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+
+
+        sc.close();
+
+    }
+}
--- src/br/com/heranca/entities/Shape.java
+++ src/br/com/heranca/entities/Shape.java
@@ -2,9 +2,22 @@ package br.com.heranca.entities;
 
 import br.com.heranca.entities.enums.Color;
 
-public class Shape {
+public abstract class Shape {
     private Color color;
 
-    
+    public Shape(){}
 
+    public Shape(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public abstract double area();
 }
